817635159ece5529780fe7c763ca0b516236b7bd
Farrukh <farrukhataev@gmail.com>
2024-02-16 10:03:33 +0300

    coffee

---
+++ script.js
@@ -0,0 +1,17 @@
+ alert('Ваня сделаешь плиз капуч? (づ ◕‿◕ )づ ')
+ var coffee = prompt('да/нет')
+ var yes = "да "
+ var no = 'нет'
+ var yes =  confirm('сделаешь?))')
+ if( yes == coffee) {
+alert('обожаю')
+ } 
+ else if (no === coffee) {
+    alert(' da  blin pojalusta sdelay uje kapuch  :( ) ')
+ }
+ alert('if (coffee empty. Refill coffee = {coffeeDrink} if (Ivan has done new coffee he molodes )))')
+    
+    
+ 
+
+ 
